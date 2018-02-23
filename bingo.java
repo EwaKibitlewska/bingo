14cc3a93c32f53aa90f1b624158107bbabbc8fba
EwaKibitlewska <malampost@gmail.com>
2018-02-23 13:18:48 +0100

    HashSet i komentarze

--- bingo.java
+++ bingo.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -7,30 +8,27 @@ public class Bingo {
 	public static void main(String[] args) {
 		Random r= new Random ();
 		Scanner odczyt= new Scanner (System.in);
-		
-		
+		Set <int> set= new HashSet <int>();
+				
 		while(true) {
 			int number= r.nextInt(75)+1;
+			set.add(number);
 			String bingo;
 			try {
 				Thread.sleep(2000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-		System.out.println("Wylosowana liczba:\n"  + number);
+		System.out.println("Wylosowana liczba:\n"  + number); // system podaje wylosowan¹ liczbê
 		{
-		System.out.println("Czy jest Bingo ?!?!");
+		System.out.println("Czy jest Bingo ?!?!"); // Sprawdzamy czy ktoœ ma bingo. Wpisujemy w konsoli nie (system dalej dzia³a) lub tak (przerywamy losowanie)
 		bingo= odczyt.nextLine();
 		System.out.println();
 		if ("tak".equals(bingo)) {
 			break;
 					
-		}
-		
-
-		
+			}
 		  }
 		}
-		}
-	
+	}
 }
